23e5a6ab417590a4ea1d56fc24edbcc89b13b00d
Shweta Bharti <shwetabharti18@gmail.com>
2018-06-26 21:37:33 -0400

    updated_code.py
    Hi Corey,
    
    I watched your video on youtube and just thought to update the code here. By the way, great video.
    
    Thanks
    Shweta

--- Object-Oriented/2-Class-Instance-Variables/oop.py
+++ Object-Oriented/2-Class-Instance-Variables/oop.py
@@ -1,6 +1,9 @@
 
 class Employee:
-
+    
+    raise_amount = 1.04             # Class Variable #
+    num_of_emps = 0                 # Class Variable #
+    
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
@@ -9,6 +12,15 @@ class Employee:
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+    
+    def apply_raise(self):
+        self.pay = int(self.pay * Employee.raise_amount)
+        #self.pay = int(self.pay * self.raise_amount)
+  
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'Employee', 60000)
+
+print emp_1.pay
+emp_1.apply_raise()
+print emp_1.pay
